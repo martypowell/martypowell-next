87823b0318d88bc7c0cff550921a90357e346ed7
Marty Powell <jpowel25@gmail.com>
2020-04-22 14:43:34 -0400

    added plpaceholder about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default (props) => (
+  <>
+    <Head>
+      <title>About Marty Powell</title>
+    </Head>
+    <div>
+      <h1>About Me</h1>
+    </div>
+  </>
+);
